7f2fae9f88eb172f109fbeab9a6d81310a42299e
Dmitrii M <demimoon18@gmail.com>
2026-04-25 12:38:15 +0400

    add lesson-9 practice

--- outputs.tf
+++ outputs.tf
@@ -16,31 +16,3 @@ output "bucket_name" {
 output "current_image_id" {
   value = data.yandex_compute_image.ubuntu_latest.id
 }
-
-# # Вывод для проверки
-# output "vm_status" {
-#   value = "VM '${yandex_compute_instance.vm.name}' created. Preemptible: ${yandex_compute_instance.vm.scheduling_policy[0].preemptible}"
-# }
-
-# # Вывод IP-адресов всех созданных машин
-# output "cluster_ips" {
-#   # Синтаксис со звездочкой [*] позволяет получить список значений для всех копий
-#   value = yandex_compute_instance.web_cluster[*].network_interface.0.nat_ip_address
-# }
-
-# outputs.tf (в корне проекта)
-
-output "frontend_public_ip" {
-  description = "Публичный IP адрес Frontend сервера"
-  value       = module.frontend.public_ip
-}
-
-output "backend_public_ip" {
-  description = "Публичный IP адрес Backend сервера"
-  value       = module.backend.public_ip
-}
-
-output "connection_string_frontend" {
-  description = "Строка для подключения по SSH"
-  value       = "ssh ubuntu@${module.frontend.public_ip}"
-}
